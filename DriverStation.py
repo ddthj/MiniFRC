df6f7c71c7e0635f47297bc7ed337f4d3237941d
ddthj <mwirsing@rthighschool.org>
2016-04-07 17:04:07 -0400

    Getting Controllers Working
    Can almost properly read the controller bits

--- DriverStation.py
+++ DriverStation.py
@@ -29,8 +29,10 @@ def loading(s,randint):
         print(" .",end="")
         time.sleep(1)
         s -=1
+    
     print("\nDetected %s joysticks: " % (pygame.joystick.get_count())+str(joysticks)+"\n")
-    return joysticks
+    thread.exit()
+    
 
 def findDevices():
     try:
@@ -49,10 +51,48 @@ def findDevices():
 
 print("MiniFRC Driver Station v1.0\n")
 print("Booting...",end="")
-joysticks = thread.start_new_thread(loading,(searchtime,1)) #grabs connected joysticks with pygame while bluetooth is still searching in the main thread
+thread.start_new_thread(loading,(searchtime,1)) #grabs connected joysticks with pygame while bluetooth is still searching in the main thread
 devices = findDevices() #finds bluetooth devices
 
+for i in range(pygame.joystick.get_count()):
+    joystick = pygame.joystick.Joystick(i)
+    joystick.init()
+    #print(str(joystick))
 
+    # Get the name from the OS for the controller/joystick
+    name = joystick.get_name()
+    print("\n\nJoystick name: %s" % (name))
+    
+    # Usually axis run in pairs, up/down for one, and left/right for
+    # the other.
+    axes = joystick.get_numaxes()
+    print("Num of axes: %s" % (axes))
+    
+    for j in range( axes ):
+        axis = joystick.get_axis( j )
+        print("Axis %s value: %s" % (j, axis) )
+        
+    buttons = joystick.get_numbuttons()
+    print("Number of buttons: %s"%(buttons))
 
-
-
+    for k in range( buttons ):
+        button = joystick.get_button( k )
+        print("Button %s value: %s"%(k,button))
+        
+    # Hat switch. All or nothing for direction, not like joysticks.
+    # Value comes back in an array.
+    hats = joystick.get_numhats()
+    print("Number of hats: %s" % (hats))
+    
+    for l in range( hats ):
+        hat = joystick.get_hat( l )
+        print("Hat %s value: %s" % (l, hat))
+if pygame.joystick.get_count() >0:
+    joystick_one = pygame.joystick.Joystick(0)
+while 1:
+    axis = joystick.get_axis(1)
+    print(axis)
+    
+  
+    
+    
