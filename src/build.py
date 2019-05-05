2ddf9a93f409628603787013cb9f8c1408e18663
ddthj <theddthj@gmail.com>
2019-05-04 21:25:01 -0400

    4.1 rebuild to fix directory mess

---
+++ src/build.py
@@ -0,0 +1,18 @@
+import sys,os
+from cx_Freeze import setup, Executable
+
+os.environ['TCL_LIBRARY'] = "C:\\Program Files (x86)\\Microsoft Visual Studio\\Shared\\Python36_64\\tcl\\tcl8.6"
+os.environ['TK_LIBRARY'] = "C:\\Program Files (x86)\\Microsoft Visual Studio\\Shared\\Python36_64\\tcl\\tk8.6"
+
+build_exe_options = {"build_exe": "C:\\Users\\Owner\\Desktop\\Code\\Repositories\\MiniFRC", "packages": ["pygame","time","serial","sys"],"excludes":["absl-py", "aiohttp", "astor", "async-timeout", "chardet", "cx-Freeze", "cycler", "Cython", "discord.py", "gast", "grpcio", "h5py ", "inputs", "Keras", "Keras-Applications", "Keras-Preprocessing", "kiwisolver", "Markdown", "matplotlib", "mock", "multidict", "numpy", "pandas", "pbr", "Pillow", "pip", "protobuf", "psutil", "py4j", "pyglet", "pyparsing", "PyQt5", "PyQt5-sip", "python-dateutil", "pytz", "PyYAML", "rlbot", "scikit-learn", "scipy",  "setuptools", "six", "sklearn", "tensorboard", "tensorflow", "tensorflow-estimator", "termcolor", "torch", "torchvision", "web.py", "websockets", "Werkzeug", "wheel"]}
+
+base = "Win32GUI" if sys.platform == "win32" else None
+
+setup(
+    name = "MiniFRC Driver Station v%s"%(4.1),
+    version = 4.1,
+    description = "Driver Station for Bluetooth-controlled robots",
+    options = {"build_exe": build_exe_options},
+    executables = [Executable("DriverStation.py",base=base)])
+    
+    
---
+++ src/test.py
@@ -0,0 +1,3 @@
+import os
+#print (os.path.abspath(os.path.join()
+print(os.path.dirname())
